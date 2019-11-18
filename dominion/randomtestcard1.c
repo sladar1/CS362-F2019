9b1a0eb9069d1051cfe3d8f0abbb4785a12b2a4e
slatonda <hokanoakaunto@gmail.com>
2019-11-17 18:44:56 -0800

    Assignment 4

--- dominion/randomtestcard1.c
+++ dominion/randomtestcard1.c
@@ -19,7 +19,7 @@ void testBaron(){
     int kCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
 
     struct gameState G;
-    int numPlayers, seed, player, coins, choice, drawNum, handNum;
+    int numPlayers, seed, player, coins, choice, drawNum, handNum, discardNum, estateNum1, estateNum2;
     int it = 1;
 
     while(it < 3500){
@@ -31,6 +31,7 @@ void testBaron(){
 
         memset(&G, 0, sizeof(struct gameState));    
         initializeGame(numPlayers, kCards, seed, &G);
+        estateNum1 = 0; estateNum2 = 0;
 
         //discard hand
         handNum = G.handCount[player];
@@ -46,16 +47,44 @@ void testBaron(){
         }
         
         G.coins = coins;
+        discardNum = G.discardCount[player];
+        discardNum++;
         coins += 4;
+
+        for(int i = 0; i < G.handCount[player]; i++) {
+            if (G.hand[player][i] == estate) { //Found an estate card!
+                estateNum1++;
+            }
+        }
+
+        if(estateNum1 > 0){
+            estateNum1 -= 1;
+        }
+
         printf("BARON TEST %d:\n", it);
-        printf("HAND NUMBER: %d\n", G.handCount[player]);
-        printf("COIN CHECK %d:", it);
+        //printf("HAND NUMBER: %d\n", G.handCount[player]);
         playBaron(player, &G, choice);
-        assertCheck(G.coins, coins);
+        if(choice > 0){
+            printf("COIN CHECK:");
+            assertCheck(G.coins, coins);            
+            for(int i = 0; i < G.handCount[player]; i++) {
+                if (G.hand[player][i] == estate) { //Found an estate card!
+                    estateNum2++;
+                }
+            }
+            
+            printf("ESTATE CHECK -1:");
+            assertCheck(estateNum1, estateNum2);
+        }
+        else{
+            printf("ESTATE CHECK +1:");
+            assertCheck(G.discardCount[player], discardNum);
+        }
+
         it++;
         printf("\n");
+        
     }
-
     printf("~BARON TEST COMPLETE!~\n");
 
 }
